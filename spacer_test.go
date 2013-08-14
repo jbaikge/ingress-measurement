3ab2f092c57de541998475ef97c3b0d97f16b2e1
jake <bigjake2s@gmail.com>
2013-08-14 19:47:15 -0400

    Finished test for first iteration of spacer

--- spacer_test.go
+++ spacer_test.go
@@ -7,8 +7,19 @@ import (
 func TestSpacerBytes(t *testing.T) {
 	// 1 word, 1 space required
 	s := NewSpacer(1, 1)
-	s.Iter() // prime
-	t.Logf("%v", s.Bytes())
+	b := s.Iter() // [[32] []]
+	if b == nil {
+		t.Fatal("Iter returned nil")
+	}
+	if l:=len(b);l != 2 {
+		t.Fatal("Invalid length: %d", l)
+	}
+	if l := len(b[0]); l != 1 {
+		t.Fatal("Invalid length of first set: %d", l)
+	}
+	if v := b[0][0]; v != ' ' {
+		t.Fatal("Invalid value for first char: %d", v)
+	}
 }
 
 func TestSingleSpacer(t *testing.T) {
