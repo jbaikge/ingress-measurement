1c1c9dafdd7be292a3b0fbc37ba4de2968c91eb0
jake <bigjake2s@gmail.com>
2013-08-15 11:54:10 -0400

    Space testing

--- spacer.go
+++ spacer.go
@@ -12,48 +12,47 @@ type Spacer struct {
 	Min, Max int // Min - Max space count for each grouping
 	Spaces   int // Spaces required to pad rest of string
 	State    []byte
-	Words    int
 }
 
 var (
 	_ sort.Interface = new(Spacer) // Ensure Spacer follows sort.Interface
 
-	EndCap    = 2
 	MaxSpaces = len(spaces)
 	spaces    = []byte("XXX")
 )
 
 func NewSpacer(words, spaces int) (s *Spacer) {
+	log.Printf("NewSpacer(%d, %d)", words, spaces)
 	s = &Spacer{
 		Min:    0,
-		Max:    MaxSpaces,
+		Max:    MaxSpaces - 1,
 		Spaces: spaces,
-		Words:  words,
 	}
-	s.State = make([]byte, (words-1)*s.Max+EndCap*2)
+	s.State = make([]byte, (words+1)*s.Max)
 	return
 }
 
 // Creates an array of slices to insert between fields. Pads between the endcaps
 // get an extra character to account for the normal word space.
 func (s *Spacer) Bytes() (b [][]byte) {
-	b = make([][]byte, s.Words+1)
+	b = make([][]byte, len(s.State)/s.Max)
 	var l int
 	for i := range b {
-		l = 0
-		for _, bit := range s.Slice(i) {
+		l = 1
+		for _, bit := range s.State[i*s.Max : (i+1)*s.Max] {
 			if bit == 1 {
 				l++
 			}
 		}
 		b[i] = spaces[:l]
 	}
+	b[0] = b[0][1:]
+	b[len(b)-1] = b[len(b)-1][1:]
 	return
 }
 
 // Initalize the state
 func (s *Spacer) Iter() [][]byte {
-	log.Printf("%v", *s)
 	for i := len(s.State) - s.Spaces; i < len(s.State); i++ {
 		s.State[i] = 1
 	}
@@ -67,13 +66,13 @@ func (s *Spacer) Next() [][]byte {
 	// Check for leading high-bits and call Next again if any are found
 	// This removes duplicates by forcing the 10's to 01's
 	var high bool
-	for i := 0; i < s.Words; i++ {
+	for i := 0; i < len(s.State); i += s.Max {
 		high = false
-		for _, bit := range s.Slice(i) {
-			if high && bit == 0 {
+		for j := i; j < i+s.Max; j++ {
+			if high && s.State[j] == 0 {
 				goto ReNext
 			}
-			high = bit == 1
+			high = s.State[j] == 1
 		}
 	}
 
@@ -81,19 +80,6 @@ func (s *Spacer) Next() [][]byte {
 
 ReNext:
 	return s.Next()
-}
-
-func (s *Spacer) Slice(i int) (slice []byte) {
-	switch i {
-	case 0:
-		slice = s.State[:EndCap]
-	case s.Words:
-		slice = s.State[len(s.State)-EndCap:]
-	default:
-		log.Printf("[%d] %d [%d : %d]", i, len(s.State), (i-1)*s.Max+EndCap, (i)*s.Max+EndCap)
-		slice = s.State[(i-1)*s.Max+EndCap : (i)*s.Max+EndCap]
-	}
-	return
 }
 
 func (s *Spacer) Len() int           { return len(s.State) }
