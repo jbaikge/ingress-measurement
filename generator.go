1f2c0e11bcbe112cbc0dcc74c6d35d8b40917d8f
jake <bigjake2s@gmail.com>
2013-08-14 22:04:51 -0400

    Completed generator

--- generator.go
+++ generator.go
@@ -4,9 +4,12 @@ import (
 	"bytes"
 )
 
+// Generates the various spacing permutations to fill a string to the desired
+// width
 type Generator struct {
 	Len    int
 	Fields [][]byte
+	Spaces [][]byte
 	spacer *Spacer
 }
 
@@ -19,12 +22,26 @@ func NewGenerator(s string, length int) (g *Generator) {
 	return
 }
 
+func (g *Generator) Bytes() (b []byte) {
+	b = make([]byte, 0, g.Len)
+	b = append(b, g.Spaces[0]...)
+	for i := 0; i < len(g.Fields); i++ {
+		b = append(b, g.Fields[i]...)
+		b = append(b, g.Spaces[i+1]...)
+	}
+	return b
+}
+
 // Reset state to beginning state
 func (g *Generator) Iter() []byte {
-	return nil
+	g.Spaces = g.spacer.Iter()
+	return g.Bytes()
 }
 
 // Return next state or nil
 func (g *Generator) Next() []byte {
+	if g.Spaces = g.spacer.Next(); g.Spaces != nil {
+		return g.Bytes()
+	}
 	return nil
 }
--- generator_test.go
+++ generator_test.go
@@ -11,17 +11,53 @@ var genTests = []struct {
 	{
 		NewGenerator("moo", 4),
 		[]string{
-			"moo ",
 			" moo",
+			"moo ",
+		},
+	},
+	{
+		NewGenerator("moo moo mr cow", 19),
+		[]string{
+			"  moo   moo  mr cow",
+			"  moo   moo mr  cow",
+			"  moo   moo mr cow ",
+			"  moo  moo   mr cow",
+			"  moo  moo mr   cow",
+			"  moo  moo mr cow  ",
+			"  moo moo   mr  cow",
+			"  moo moo   mr cow ",
+			"  moo moo  mr   cow",
+			"  moo moo  mr cow  ",
+			"  moo moo mr   cow ",
+			"  moo moo mr  cow  ",
+			" moo   moo   mr cow",
+			" moo   moo mr   cow",
+			" moo   moo mr cow  ",
+			" moo moo   mr   cow",
+			" moo moo   mr cow  ",
+			" moo moo mr   cow  ",
+			"moo   moo   mr  cow",
+			"moo   moo   mr cow ",
+			"moo   moo  mr   cow",
+			"moo   moo  mr cow  ",
+			"moo   moo mr   cow ",
+			"moo   moo mr  cow  ",
+			"moo  moo   mr   cow",
+			"moo  moo   mr cow  ",
+			"moo  moo mr   cow  ",
+			"moo moo   mr   cow ",
+			"moo moo   mr  cow  ",
+			"moo moo  mr   cow  ",
 		},
 	},
 }
 
 func TestGenerator(t *testing.T) {
 	for _, test := range genTests {
-		t.Logf("%v", test.G)
-		for v := test.G.Iter(); v != nil; v = test.G.Next() {
-			t.Logf("%v", v)
+		for i, v := 0, test.G.Iter(); v != nil; i, v = i+1, test.G.Next() {
+			if exp := test.Exp[i]; exp != string(v) {
+				t.Errorf("Expected '%s'; Got '%s'", exp, v)
+			}
 		}
 	}
 }
